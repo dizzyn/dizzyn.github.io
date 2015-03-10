19e80403b0ca2bfc02c93887404d6b62a5811941
Tomas Randus <tom.randus@gmail.com>
2015-03-10 08:14:54 +0100

    cv

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    less: {
+      portfolio: {
+        files: {
+          'css/style.css': 'css/style.less'
+        }
+      },
+      cv: {
+        files: {
+          'cv/css/style.css': 'cv/css/style.less'
+        }
+      }
+    },
+    watch: {
+      sass_portfolio: {
+        files: 'css/**/*.less',
+        tasks: ['less:portfolio'],
+        options: {
+          atBegin: true
+        }
+      },
+      sass_cv: {
+        files: 'cv/css/**/*.less',
+        tasks: ['less:cv'],
+        options: {
+          atBegin: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less']);
+
+};
